5c4ac54671668c2ba2fc1c043c158ee62107ad90
Sneakyguy6 <57048867+Sneakyguy6@users.noreply.github.com>
2020-03-31 13:40:16 +0100

    adding project

---
+++ src/main/java/net/sneak/mcbr/Plugin.java
@@ -0,0 +1,15 @@
+package net.sneak.mcbr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
