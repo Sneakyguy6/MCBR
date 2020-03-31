046558dc9b0a6bc56e139ee57c87f196e72b1e4a
Sneakyguy6 <57048867+Sneakyguy6@users.noreply.github.com>
2020-03-31 13:42:33 +0100

    initial plugin commit

--- src/main/java/net/sneak/mcbr/Plugin.java
+++ src/main/java/net/sneak/mcbr/Plugin.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Plugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
-
+		
 	}
 
 	@Override
 	public void onDisable() {
-
+		
 	}
 }
