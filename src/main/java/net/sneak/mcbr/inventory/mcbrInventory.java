45d0ad0ce0fe70e5ac1b5f4f97bd11ede3860176
Sneakyguy6 <57048867+Sneakyguy6@users.noreply.github.com>
2020-04-12 23:50:24 +0100

    restricted inventory space (will need proper testing)

--- src/main/java/net/sneak/mcbr/inventory/mcbrInventory.java
+++ src/main/java/net/sneak/mcbr/inventory/mcbrInventory.java
@@ -1,33 +1,48 @@
 package net.sneak.mcbr.inventory;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class mcbrInventory implements Listener {
-	@EventHandler
+	/*@EventHandler
 	public void onPlayerPickup(EntityPickupItemEvent e) {
 		if(!(e.getEntity() instanceof Player))
 			return;
 		Player p = (Player) e.getEntity();
 		boolean full = true;
 		for(int i = 0; i < 9; i++) {
-			if(p.getInventory().getItem(i) != null) {
+			if(p.getInventory().getContents()[i] == null) {
 				full = false;
 				break;
 			}
 		}
 		if(full)
 			e.setCancelled(true);
-	}
+	}*/
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onItemClick(InventoryClickEvent e) {
-		if(e.getSlot() >= 9)
+		if((e.isRightClick() || e.isLeftClick() || e.getAction().equals(InventoryAction.HOTBAR_SWAP)) && e.getSlot() >= 9)
 			e.setCancelled(true);
-		//if(e.getAction().equals(InventoryAction.Sw))
+		if(e.getAction().equals(InventoryAction.HOTBAR_MOVE_AND_READD) || e.isShiftClick() || e.getClick().equals(ClickType.DOUBLE_CLICK))
+			e.setCancelled(true);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onJoin(PlayerJoinEvent e) {
+		ItemStack item = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(" ");
+		item.setItemMeta(meta);
+		for(int i = 9; i < 36; i++)
+			e.getPlayer().getInventory().setItem(i, item);
 	}
 }
