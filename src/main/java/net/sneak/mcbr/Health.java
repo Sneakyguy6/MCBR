6967f6eac4949d22a4b4cbc1b43af0bad9092208
Sneakyguy6 <57048867+Sneakyguy6@users.noreply.github.com>
2020-04-29 18:12:21 +0100

    health now has a cooldown

--- src/main/java/net/sneak/mcbr/Health.java
+++ src/main/java/net/sneak/mcbr/Health.java
@@ -1,21 +1,40 @@
 package net.sneak.mcbr;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class Health implements Listener {
+	private Map<Player, Long> cooldowns;
+	
+	public Health() {
+		this.cooldowns = new HashMap<Player, Long>();
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		this.cooldowns.put(e.getPlayer(), 0L);
+	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onClick(PlayerInteractEvent e) {
-		if(e.getItem() == null)
+		if(e.getItem() == null || e.getPlayer().getHealth() == 20)
 			return;
-		if(e.getPlayer().getHealth() == 20)
+		if(this.cooldowns.get(e.getPlayer()) > System.currentTimeMillis()) {
+			e.getPlayer().sendMessage(ChatColor.GREEN + "You cannot eat for " + ChatColor.DARK_GREEN + ((this.cooldowns.get(e.getPlayer()) - System.currentTimeMillis()) / 1000) + ChatColor.GREEN + " seconds" );
 			return;
+		}
 		e.setCancelled(true);
 		double health = e.getPlayer().getHealth();
 		switch(e.getItem().getType())
@@ -42,5 +61,6 @@ public class Health implements Listener {
 		e.getItem().setAmount(e.getItem().getAmount() - 1);
 		Bukkit.getWorlds().get(0).playSound(e.getPlayer().getLocation(), Sound.ENTITY_GENERIC_EAT, 1, 1);
 		Bukkit.getWorlds().get(0).spawnParticle(Particle.HEART, e.getPlayer().getLocation().add(0, 2, 0), 20);
+		this.cooldowns.put(e.getPlayer(), System.currentTimeMillis() + 3000);
 	}
 }
